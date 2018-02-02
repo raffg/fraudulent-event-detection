f4282c220b555fe4f0cd0abce8d378d68a1dbd43
Rohit Unni <rohitunni11@gmail.com>
2018-02-01 20:34:37 -0800

    further debugged predict.py

--- predict.py
+++ predict.py
@@ -4,24 +4,27 @@ from logistic_regression import featurize
 import pickle
 import json
 
-def main(model_file, input_data_file):
+def main(model, data):
     '''
     Unpickles model as passed in
     Takes data point in json file, creates necessary features, and
     passes into model to make fraud prediction
     '''
-    with open(model_file) as f_mod:
+
+
+    with open(model) as f_mod:
         model = pickle.load(f_mod)
 
-    with open(input_data_file) as f_data:
-        input_data = json.load(f_data)
-
     # pass data through same featurizing that training data went through
-    data_featurized = featurize(pd.read_json(input_data))
-    # makes prediction from model
-    prediction = model.predict_proba(data_featurized)
+    df = pd.read_json(data)
+    data_featurized = featurize(df)[0]
 
-    print "Percent chance of fraud: {}".format(prediction * 100)
+    # makes prediction from model
+    for row in data_featurized:
+
+        prediction = model.predict_proba(row)
+
+        print "Percent chance of fraud: {}".format(prediction * 100)
 
 
 
